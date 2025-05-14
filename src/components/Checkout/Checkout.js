1931f1c0ed70ac959b6797cd65cbad57f847d760
pilae <pilargomez.dev@gmail.com>
2025-05-14 16:58:14 -0300

    revision id compra

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import "./Checkout.css";
 
 export default function Checkout() {
@@ -9,6 +9,10 @@ export default function Checkout() {
       <h3>¡Gracias Por Tu Compra!</h3>
       <h4>Te contactaremos con los datos que nos dejaste.</h4>
       <h4>Tu código de compra es: <strong>{orderid}</strong></h4>
+
+      <Link to="/home" style={{display: "block", margin: "25px", textAlign: "center"}}>
+         <button className="btn-primary">Volver al Inicio</button>
+      </Link>
     </div>
   );
 }
